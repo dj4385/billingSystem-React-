1940507ad5cceaff46518041a63723f814cc0025
dheeraj sharma <dj4385@gmail.com>
2019-11-28 11:16:00 +0530

    for loop

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,19 +1,57 @@
 import React from 'react'
+import Axios from 'axios'
 
-export const Product = (props)=>{
+export default class Product extends React.Component{
     
-    // var productJsx = props.productRes.map((ele,i)=>{
-    //     return <div key={i}>
-    //             <p><span className="h4">{ele.name}</span></p>
-    //         </div>
-    //     })
+    constructor(props){
+        super(props)
+        this.product = ""
+        this.state = {
+            productList : this.product
+        }
+    }
 
-    console.log('ProductJSx',props.productRes)
+    componentWillMount(){
+        this.getProductList()
+    }
 
-    return(
-        <>
-            <h1>Product</h1>
-            {/* {productJsx()} */}
-        </>
-    )
+    getProductList(){
+        var _url = 'http://localhost:8080/products'
+        Axios({
+            method: 'GET',
+            url: _url
+        }).then((apiRes)=>{
+            this.product = apiRes.data
+            this.setState({
+                productList: this.product
+            },()=>console.log("state val",this.state.productList))
+        }).catch(err=>console.log(err))
+    }
+    
+    productJsx(){
+        for(let i=0;i<this.state.productList.length;i++){
+            return <div key={i}>
+                <p>
+                    <span className="h4"><b>Product Name:</b> {this.state.productList[i].name}</span><br/>
+                    <span><b>Category:</b> {this.state.productList[i].category}</span><br/>
+                    <span><b>Price:</b>{this.state.productList[i].price}</span><br/>
+                </p>
+             </div>
+        } 
+        // this.state.productList.map((ele,i)=>{
+        // console.log("ele",this.state.productList)
+        // return <div key={i}>
+        //         <p><span className="h4">{ele.name}</span></p>
+        //     </div>
+        // })
+    }
+
+    render(){
+        return(
+            <>
+                <h1>Product</h1>
+                {this.productJsx()}
+            </>
+        )
+    }
 }
